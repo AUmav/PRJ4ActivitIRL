ece87bc6334dea9d63eede8b41c8116893669261
AUmav <90242510+AUmav@users.noreply.github.com>
2022-03-14 13:52:31 +0100

    Changed navigation bar CSS

--- activitirl-web/src/components/NavBar.js
+++ activitirl-web/src/components/NavBar.js
@@ -1,10 +1,14 @@
 const NavBar = () => {
     return(
         <div className="navigation-bar">
-            <h1>ActivityIRL</h1>
-            <a href="/faq">FAQ</a>
-            <a href="/join">Opret bruger</a>
-            <a href="/login">Log ind</a>
+            <div className="nav-left">
+                <h1>ActivityIRL</h1>
+            </div>
+            <div className="nav-right">            
+                <a href="/faq">FAQ</a>
+                <a href="/join">Opret bruger</a>
+                <a href="/login">Log ind</a>
+            </div>
         </div>
         
     );
