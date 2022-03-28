3b2ea5f8558adfb407d4991fd062894862ef3158
AUmav <90242510+AUmav@users.noreply.github.com>
2022-03-28 13:33:20 +0200

    Fixed navigation bar text alignment

--- activitirl-web/src/components/NavBar.js
+++ activitirl-web/src/components/NavBar.js
@@ -3,14 +3,19 @@ import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const NavBar = () => {
     return(
         <div className="navigation-bar">
-            <div className="nav-left">
-                <Link to="/"><h1>ActivityIRL</h1></Link>
+            <Link to="/"><h1>ActivityIRL</h1></Link>
+            <div className="nav">
+                <li>
+                    <Link to="/faq">FAQ</Link>
+                </li>
+                <li>
+                    <Link to="/join">Opret bruger</Link>
+                </li>
+                <li>
+                    <Link to="/login">Log ind</Link>
+                </li>
             </div>
-            <div className="nav-right">         
-                <Link to="/faq">FAQ</Link>
-                <Link to="/join">Opret bruger</Link>
-                <Link to="/login">Log ind</Link>
-            </div>
+            
         </div>
         
     );
