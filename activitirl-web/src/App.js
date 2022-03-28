7fbb8c6066a2c5ab5f8c8202ba380b9555255678
AUmav <90242510+AUmav@users.noreply.github.com>
2022-03-28 13:02:54 +0200

    Set up routing

--- activitirl-web/src/App.js
+++ activitirl-web/src/App.js
@@ -1,17 +1,34 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom"
+
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar'
 import InfoElement from './components/InfoElement';
 import PostContainer from './components/PostContainer';
 
+
+
 function App() {
   return (
-    <div className="App">
-    <NavBar/>
-    <InfoElement/>
-    <PostContainer/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <InfoElement/>
+          <Routes>
+            <Route path="/" element={<PostContainer/>} />
+            <Route path="/faq" element={<FAQ/>} />
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
+const FAQ = () => {
+  return <h2>hahah</h2>
+}
 export default App;
--- activitirl-web/src/components/NavBar.js
+++ activitirl-web/src/components/NavBar.js
@@ -1,18 +1,22 @@
+import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const NavBar = () => {
     return(
         <div className="navigation-bar">
             <div className="nav-left">
-                <h1>ActivityIRL</h1>
+                <Link to="/"><h1>ActivityIRL</h1></Link>
             </div>
-            <div className="nav-right">            
-                <a href="/faq">FAQ</a>
-                <a href="/join">Opret bruger</a>
-                <a href="/login">Log ind</a>
+            <div className="nav-right">         
+                <Link to="/faq">FAQ</Link>
+                <Link to="/join">Opret bruger</Link>
+                <Link to="/login">Log ind</Link>
             </div>
         </div>
         
     );
-
 }
 
+const FAQ = () => {
+    return <h2>FAQ</h2>
+}
 export default NavBar
