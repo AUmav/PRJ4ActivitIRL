7912b3a38db5ac128ca4d75cf86e7646dea0deca
Yevhen <yevhen89@gmail.com>
2022-05-04 12:57:31 +0200

    null-added

--- activitirlApi/activitirlApi/Models/DTOs/EventPutDTO.cs
+++ activitirlApi/activitirlApi/Models/DTOs/EventPutDTO.cs
@@ -15,7 +15,7 @@ namespace ActivitIRLApi.Models.DTOs
         public string? City { get; set; }
         public string? ZipCode { get; set; }
         public string? State { get; set; }
-        public string Country { get; set; }
+        public string? Country { get; set; }
 
         public string? MinAge { get; set; }
         public string? MaxAge { get; set; }
