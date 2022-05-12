ce33e6b5d08c5ead6972b6d532a530834d129880
thea2805 <thea2805@gmail.com>
2022-05-12 15:20:41 +0200

    Pages correctly showing dateTime

--- activitirl-web/src/components/01-molecules/activityFewDetails/ActivityFewDetails.js
+++ activitirl-web/src/components/01-molecules/activityFewDetails/ActivityFewDetails.js
@@ -1,13 +1,41 @@
 import TitleText from "../../00-atoms/text/TitleText"
 
 const ActivityFewDetails = ({data}) => {
-    //console.log("activity", data)
+     
+    
+
+    function dateFormat(date)
+    {
+        let dateTime = new Date(date);
+        let tempDate = dateTime.getDate();
+        let tempMonth = dateTime.getMonth();
+        let tempYear = dateTime.getFullYear();
+        let tempHours = dateTime.getHours();
+        let tempMinutes = dateTime.getMinutes();
+
+        // The getMonth function starts at zero, and so to get the correct month must be incremented
+        // The function does not add a zero before the number.. this is also manually added
+        tempMonth++;
+        if(tempMonth<10)
+        {
+            tempMonth = "0"+tempMonth;        }
+
+
+        let formattedDateTime = tempDate + "-" + tempMonth + "-" + tempYear + " kl. " + tempHours + ":" + tempMinutes;
+        console.log(formattedDateTime);
+        return formattedDateTime;
+    }
+
+    let dateTime = dateFormat(data.date);
+    console.log(data);
+
+
     return (
         <div className="activity-details">
            <TitleText title={data.title}/>
             <p>{data.activity}</p>
             <p>{data.zipCode} {data.city}</p>
-            <p>Dato</p>
+            <p>{dateTime}</p>
         </div>
     )
 }
